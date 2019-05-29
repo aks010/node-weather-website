c5ea002b71b56af1c13bb644e508ddc9910efa7e
Ashish Kumar Singh <code.aks.010@gmail.com>
2019-05-30 02:51:14 +0530

    Display searched location

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,21 @@
 
-const weatherForm = document.querySelector('form')
+const weatherForm = document.querySelector('button')
 const search = document.querySelector('input')
+const displayWeather = document.querySelector('.displayWeather')
 
 weatherForm.addEventListener("click",(e) => {
     e.preventDefault()
     const location = search.value
+    let markup
+    // console.log(location)
+    displayWeather.innerHTML = "Loading..."
     fetch('/weather?address='+location).then((response)=> {
     response.json().then((data) => {
         if(data.error) {
-            return console.log(data.error)
+            return displayWeather.innerHTML = `<div style="color:white; background: red; padding: 10px; width: max-content; border-radius: 0.1rem">${data.error}</div>`
         }
-        console.log(data)
+        displayWeather.innerHTML = `<div>Location: ${data.location}</div><div>ForeCast: ${data.forecastData}</div>`
     })
 })
 })
-console.log(search.value)
+// console.log(search.value)
