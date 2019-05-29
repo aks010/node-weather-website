18c17422eb9d30e3a35c908f9a1b582ff03f9916
Ashish Kumar Singh <code.aks.010@gmail.com>
2019-05-29 10:24:56 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ const search = document.querySelector('input')
 weatherForm.addEventListener("click",(e) => {
     e.preventDefault()
     const location = search.value
-    fetch('http://localhost:3000/weather?address='+location).then((response)=> {
+    fetch('/weather?address='+location).then((response)=> {
     response.json().then((data) => {
         if(data.error) {
             return console.log(data.error)
